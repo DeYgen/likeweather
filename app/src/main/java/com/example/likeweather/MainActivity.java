89871769f9c007f60ace208833e7c19a6aaffa15
DeYgen <931219239@qq.com>
2020-12-23 20:22:10 +0800

    选择城市查看天气，但有小问题

--- app/src/main/java/com/example/likeweather/MainActivity.java
+++ app/src/main/java/com/example/likeweather/MainActivity.java
@@ -7,13 +7,20 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.likeweather.gson.Weather;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather",null)!=null){
+            Intent intent=new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
 }
--- app/src/main/java/com/example/likeweather/util/Utility.java
+++ app/src/main/java/com/example/likeweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.likeweather.db.City;
 import com.example.likeweather.db.County;
 import com.example.likeweather.db.Province;
+import com.example.likeweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -68,5 +70,17 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent =jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
